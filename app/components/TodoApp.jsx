939e4003b792e48612738fd48f1e99e3e8b6e003
Matt Black <matthewcoggin@gmail.com>
2016-05-18 00:02:12 -0400

    Wired up handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,6 +3,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -11,16 +12,16 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog'
         }, {
-          id: 2,
+          id: uuid(),
           text: 'Mow the yard'
         }, {
-          id: 3,
+          id: uuid(),
           text: 'Get mail'
         }, {
-          id: 4,
+          id: uuid(),
           text: 'Love wife'
         }
       ]
@@ -28,7 +29,15 @@ var TodoApp = React.createClass({
   },
 
   handleAddTodo: function (text) {
-    alert('new todo: ' + text);
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    });
   },
 
   handleSearch: function (showCompleted, searchText) {
