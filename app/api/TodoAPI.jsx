6ef0b6278384077a74ad6bed764247ee92eee3f8
Matt Black <matthewcoggin@gmail.com>
2016-05-18 01:59:28 -0400

    Add search and filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -7,6 +7,7 @@ module.exports = {
         return todos;
       }
     },
+
     getTodos: function () {
       var stringTodos = localStorage.getItem('todos');
       var todos = [];
@@ -18,6 +19,33 @@ module.exports = {
       }
 
       return $.isArray(todos) ? todos : [];
+    },
+
+    filterTodos: function (todos, showCompleted, searchText) {
+      var filteredTodos = todos;
+
+      // Filter by showCompleted
+      filteredTodos = filteredTodos.filter((todo) => {
+        return !todo.completed || showCompleted;
+      });
+
+      // Filter by searchText
+      filteredTodos = filteredTodos.filter((todo) => {
+        return todo.text.toLowerCase().indexOf(searchText) >= 0 || searchText === '';
+      });
+
+      // Sort todos with non-completed first
+      filteredTodos.sort((a, b) => {
+        if (a.completed === false && b.completed === true) {
+          return -1;
+        } else if (a.completed === true && b.completed === false) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      return filteredTodos;
     }
 
 };
